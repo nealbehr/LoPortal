f1e77473cdb1638e7513e0cd1910652f476ff4f8
s.samoilenko <serhii.samoilenko@appsorama.com>
2015-05-12 13:28:16 +0300

    deploy

--- .ebextensions/symlink.sh
+++ .ebextensions/symlink.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # sudo yum install -y nodejs npm --enablerepo=epel
 
-sudo npm install -y -g npm
+#sudo npm install -y -g npm
 
-mkdir -p /home/ec2-user/node_modules
+#mkdir -p /home/ec2-user/node_modules
 
-ln -s /home/ec2-user/node_modules/ node_modules
+#ln -s /home/ec2-user/node_modules/ node_modules
 
 eval "ln -s ../config/"$1".yml config/config.yml"
 
