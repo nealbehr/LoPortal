c5e151899fbdc8672e2e58f282f4c9d2c3d0eb30
s.samoilenko <serhii.samoilenko@appsorama.com>
2015-05-12 15:17:44 +0300

    Revert " deploy"
    This reverts commit 068aa26a94a5afc610b38fc59da1be88943a29f4.

--- .ebextensions/symlink.sh
+++ .ebextensions/symlink.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-# sudo yum install -y nodejs npm --enablerepo=epel
+sudo yum install -y nodejs npm --enablerepo=epel
 
 sudo npm install -y -g npm
 
 mkdir -p /home/ec2-user/node_modules
-
 ln -s /home/ec2-user/node_modules/ node_modules
 
 eval "ln -s ../config/"$1".yml config/config.yml"
