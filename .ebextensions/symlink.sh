b9c967050dec9f725f8e7a3bdecaa83b8c365a44
s.samoilenko <serhii.samoilenko@appsorama.com>
2015-05-12 15:17:57 +0300

    Revert "deploy"
    This reverts commit 3f25b17667ab8e5c69d1e82ca530cb5bf431aac4.

--- .ebextensions/symlink.sh
+++ .ebextensions/symlink.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-curl -sL https://rpm.nodesource.com/setup | bash -
-
-yum install -y nodejs
-
-npm install -y -g npm
-
 mkdir -p /home/ec2-user/node_modules
 ln -s /home/ec2-user/node_modules/ node_modules
 
