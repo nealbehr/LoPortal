083a110f1edf9cf48274f37d61d9e8db2d3968f6
Dmitry Kobyleha <dmitry@appsorama.com>
2015-05-30 13:06:34 +0300

    Fixed issue with unused angualr module

--- web/js/app.js
+++ web/js/app.js
@@ -2,7 +2,7 @@
     "use strict";
     settings = settings || {};
 
-    var app = angular.module('loApp', ['ngRoute', 'helperService', 'dashboardModule', 'authModule', 'ngCookies', 'requestModule', 'userModule', 'userProfileModule', 'adminModule', 'lenderModule', 'realtyCompanyModule', 'ngDialog', 'requestFlyerModule', 'approvalModule', 'ngAutocomplete', 'vsGoogleAutocomplete']);
+    var app = angular.module('loApp', ['ngRoute', 'helperService', 'dashboardModule', 'authModule', 'ngCookies', 'requestModule', 'userModule', 'userProfileModule', 'adminModule', 'lenderModule', 'realtyCompanyModule', 'ngDialog', 'requestFlyerModule', 'approvalModule', 'vsGoogleAutocomplete']);
 
     app.constant('HTTP_CODES', {FORBIDDEN: 403});
     app.constant('TOKEN_KEY', 'access_token');
