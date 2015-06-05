8cfbd69c9a195ebf331a43d34cf0d2ce21d9e464
Dmitry Kobyleha <dmitry@appsorama.com>
2015-06-05 13:43:54 +0300

    Fixed wkhtml install in vagrant

--- puppet/bootstrap.sh
+++ puppet/bootstrap.sh
@@ -4,5 +4,4 @@ sudo rpm -ivh https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release
 sudo yum install -y puppet
 
 cd /tmp
-wget http://downloads.sourceforge.net/wkhtmltopdf/wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm
-sudo yum install wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm
+sudo rpm -qa | grep wkhtmltox || sudo yum -y install http://downloads.sourceforge.net/wkhtmltopdf/wkhtmltox-0.12.2.1_linux-centos6-amd64.rpm
