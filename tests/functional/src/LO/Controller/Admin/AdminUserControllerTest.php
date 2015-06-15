e38d794f514522016c938a97ccbccf84001c55d2
Eugene Lysenko <zhenyaaynehz@gmail.com>
2015-06-15 11:18:01 +0300

    Refactoring code

--- tests/functional/src/LO/Controller/Admin/AdminUserControllerTest.php
+++ tests/functional/src/LO/Controller/Admin/AdminUserControllerTest.php
@@ -1,11 +1,15 @@
 <?php namespace LO\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Silex\WebTestCase;
+use \LO\Model\Entity\User;
+use \LO\Model\Entity\Address;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminUserControllerTest extends WebTestCase
 {
     private $user;
+
+    private $showDeprecated = false;
 
     public function createApplication() {
         $app = (new \LO\Application(['prod', 'test'], __DIR__.'/../../../../../../config/'))->bootstrap()->initRoutes();
@@ -19,69 +23,67 @@ class AdminUserControllerTest extends WebTestCase
 
     public function setUp() {
         parent::setUp();
-        error_reporting(E_ALL ^ E_USER_DEPRECATED);
+
+        if (!$this->showDeprecated) {
+            error_reporting(E_ALL ^ E_USER_DEPRECATED);
+        }
+
         $this->app->getEntityManager()->getConnection()->executeQuery(
             file_get_contents(__DIR__.'/../../../../dump.sql')
         );
         $this->user = $this->getUserData();
     }
 
-    public function getUserData()
-    {
-        return array(
-            'user' => array(
-                'isLogged' => 'false',
-                'sales_director' => '',
-                'sales_director_email' => '',
-                'sales_director_phone' => '',
-                'password' => array(
-                    'password' => '',
-                    'password_confirm' => '',
-                ),
-                'roles' => array('ROLE_USER'),
-                'switched' => 'false',
-                'address' => array(
-                    'place_id' => 'ChIJ_7mUR165woARxQlc1V2IvEk',
-                    'street' => 'Santa Monica Blvd',
-                    'city' => 'Los Angeles',
-                    'state' => 'CA',
-                    'formatted_address' => 'Santa Monica Blvd, Los Angeles, CA, USA'
-                ),
-                'addressOptions' => array(
-                    'types' => array('geocode'),
-                    'componentRestrictions' => array('country' => 'US')
-                ),
-                'lender' => array(
-                    'id' => '1',
-                    'name' => 'Banc Home Loans'
-                ),
-                'first_name' => 'First',
-                'last_name' => 'Last',
-                'email' => 'test@test.com'
-            )
-        );
+    private function getUserData() {
+        $user    = new User();
+        $address = new Address;
+
+        $user->setRoles(['ROLE_USER']);
+        $user->setLender([
+            'id'   => '1',
+            'name' => 'Banc Home Loans'
+        ]);
+        $user->setFirstName('First');
+        $user->setLastName('Last');
+        $user->setEmail('test@test.com');
+
+        $address->setPlaceId('ChIJ_7mUR165woARxQlc1V2IvEk');
+        $address->setStreet('Santa Monica Blvd');
+        $address->setCity('Los Angeles');
+        $address->setState('CA');
+        $address->setFormattedAddress('Santa Monica Blvd, Los Angeles, CA, USA');
+        $user->setAddress($address);
+
+        $user            = $user->toArray();
+        $user['address'] = $user['address']->toArray();
+
+        return ['user' => $user];
     }
 
     public function testAddUser() {
         $controller = new Admin\AdminUserController();
         $request    = new Request();
 
-        $this->assertEquals(
-            '{"id":"101"}',
-            $controller->addUserAction($this->app, $request->create('/admin/user', 'POST', $this->user))->getContent()
+        $data = json_decode(
+            $controller->addUserAction($this->app, $request->create('/admin/user', 'POST', $this->user))->getContent(),
+            true
         );
+
+        $this->assertEquals('101', $data['id']);
     }
 
-    public function testAddUserWithInvalidPhone() {
+    public function testEmailAddressIsAlreadyRegistered() {
         $controller = new Admin\AdminUserController();
         $request    = new Request();
 
         $data                  = $this->user;
         $data['user']['email'] = 'admin@1rex.com';
 
-        $this->assertEquals(
-            '{"form_errors":["","Email address is already registered.\n"],"message":"User info not valid."}',
-            $controller->addUserAction($this->app, $request->create('/admin/user', 'POST', $data))->getContent()
+        $data = json_decode(
+            $controller->addUserAction($this->app, $request->create('/admin/user', 'POST', $data))->getContent(),
+            true
         );
+
+        $this->assertEquals(true, isset($data['form_errors']));
     }
-} 
+}
