527aa987b5f846db7e5ba54c78cd4d89903643c5
Eugene Lysenko <zhenyaaynehz@gmail.com>
2015-07-08 12:39:04 +0300

    Add statys table and statys data

---
+++ migrations/20150708084452_create_status_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateStatusTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+     */
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->execute("CREATE TABLE `statys` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `type` enum('approve',"
+            ."'decline') DEFAULT NULL, `name` varchar(50) DEFAULT NULL, `text` text, PRIMARY KEY (`id`)) ENGINE=InnoDB "
+            ."DEFAULT CHARSET=utf8;");
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->dropTable('statys');
+    }
+}
