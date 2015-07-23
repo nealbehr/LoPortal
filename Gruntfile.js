629402e1fa6a21235bfed9eeb4cafe35bd35d8b5
Eugene Lysenko <zhenyaaynehz@gmail.com>
2015-07-23 18:27:20 +0300

    Refactoring grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,46 @@ module.exports = function(grunt) {
     grunt.nameJsMinFile = 'build/scripts.min.' + grunt.versionFiles + '.js';
     grunt.nameCSSMinFile = 'build/css.min.' + grunt.versionFiles + '.css';
 
-    // Задачи
+    // Configurable paths for the application
+    var appConfig = {
+        dist: 'dist'
+    };
+
+    // Tasks
     grunt.initConfig({
+        // Project settings
+        yeoman: appConfig,
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= yeoman.dist %>/{,*/}*',
+                        '!<%= yeoman.dist %>/.git{,*/}*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+
+        ebDeploy: {
+            options: {
+                region: 'us-west-1',
+                application: 'first-rex-lo-portal',
+                environment: 'firstRexLoPortal-stage',
+                profile: 'eb-cli-client'
+            },
+            stage: {
+                files: [
+                    { src: ['.ebextensions/*'] },
+                    { cwd: '<%= yeoman.dist %>/', src: ['**'], expand: true }
+                ]
+            }
+        },
+
         concat: {// Склеиваем
             options: {
                 separator: ';'
@@ -25,7 +63,7 @@ module.exports = function(grunt) {
                     'web/js/service/*.js',
                     'web/js/*.js'
                 ],
-                dest: 'web/build/scripts.js'
+                dest: '<%= yeoman.dist %>/web/build/scripts.js'
             }
         },
         uglify: {// Сжимаем
@@ -36,14 +74,14 @@ module.exports = function(grunt) {
                 files: {
                     // Результат задачи concat
                     //'web/build/scripts.min.js': '<%= concat.main.dest %>'
-                    'web/<%= grunt.nameJsMinFile %>': '<%= concat.main.dest %>'
+                    '<%= yeoman.dist %>/web/<%= grunt.nameJsMinFile %>': '<%= concat.main.dest %>'
                 }
             }
         },
         processhtml:{
             dist: {
                 files: {
-                    'view/index.twig': ['view/index.twig']
+                    '<%= yeoman.dist %>/view/index.twig': ['<%= yeoman.dist %>/view/index.twig']
                 },
                 options: {
                     data: {
@@ -54,7 +92,7 @@ module.exports = function(grunt) {
         'string-replace': {
             version: {
                 files: {
-                    'view/index.twig': ['view/index.twig']
+                    '<%= yeoman.dist %>/view/index.twig': ['view/index.twig']
                 },
                 options: {
                     replacements: [{
@@ -77,7 +115,7 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: 'web/images/',
                     src: ['**/*.{png,jpg,gif,svg}'],
-                    dest: 'web/images/'
+                    dest: '<%= yeoman.dist %>/web/images/'
                 }]
             }
         },
@@ -90,11 +128,11 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: 'web/css',
                     src: ['*.css', '!*.min.css'],
-                    dest: 'web/build/css',
+                    dest: '<%= yeoman.dist %>/web/build/css',
                     ext: '.min.css'
                 },
                 {
-                    'web/<%= grunt.nameCSSMinFile %>': [
+                    '<%= yeoman.dist %>/web/<%= grunt.nameCSSMinFile %>': [
                         'web/css/jquery-ui.structure.min.css',
                         'web/css/jquery-ui.min.css',
                         'web/css/bootstrap.min.css',
@@ -113,6 +151,8 @@ module.exports = function(grunt) {
     //'<%= cssmin.target.files[0].dest %>/all.min.css',
                             //src: ['*.css', '!*.min.css'],
     // Загрузка плагинов, установленных с помощью npm install
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-eb-deploy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-processhtml');
@@ -126,9 +166,26 @@ module.exports = function(grunt) {
         'uglify',
         'string-replace',
         'processhtml',
-//        'imagemin',
-        'cssmin']
-    );
+//       'imagemin',
+        'cssmin'
+    ]);
+
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'concat',
+        'uglify',
+        'string-replace',
+        'processhtml',
+        'imagemin',
+        'cssmin'
+    ]);
+
+
+    grunt.registerTask('deploy', [
+        'build',
+        'ebDeploy'
+    ]);
 };
 
 //"grunt-contrib-imagemin": "^0.9.2",
