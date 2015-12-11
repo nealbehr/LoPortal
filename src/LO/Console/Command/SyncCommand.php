5aff85fc1a49f33e64fbee69341076b1633b4346
Eugene Lysenko <zhenyaaynehz@gmail.com>
2015-12-11 23:35:16 +0200

    Refactoring sync process

--- src/LO/Console/Command/SyncCommand.php
+++ src/LO/Console/Command/SyncCommand.php
@@ -8,13 +8,13 @@
 namespace LO\Console\Command;
 
 use LO\Application;
+use LO\Model\Entity\User;
+use LO\Model\Entity\Lender;
+use LO\Model\Entity\Address;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use LO\Model\Entity\User;
-use LO\Model\Entity\Lender;
 use \BaseCRM\Client;
 use \BaseCRM\Sync;
 use Doctrine\ORM\NoResultException;
@@ -27,11 +27,13 @@ class SyncCommand extends Command
 
     private $entityManager;
 
-    function __construct(Application $app, $entityManager)
+    private $sync;
+
+    function __construct(Application $app)
     {
         parent::__construct();
         $this->app           = $app;
-        $this->entityManager = $entityManager;
+        $this->entityManager = $app->getEntityManager();
         $this->sync          = new Sync(
             (new Client(['accessToken' => $this->app->getConfigByName('basecrm', 'accessToken')])),
             $this->app->getConfigByName('basecrm', 'devicesUuid')
@@ -40,29 +42,26 @@ class SyncCommand extends Command
 
     protected function configure()
     {
-        $this->setName('portal:sync')
-            ->setDescription('Sync with BaseCRM')
-            ->addArgument(
-                'name',
-                InputArgument::OPTIONAL,
-                'It may take some time'
-            );
+        $this->setName('portal:sync')->setDescription('Sync with BaseCRM');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $qUser = $this->entityManager
-            ->getRepository(User::class)
-            ->createQueryBuilder('u')
-            ->where('u.base_id = :id');
+        $qUser   = $this->entityManager->getRepository(User::class)->createQueryBuilder('u')->where('u.base_id = :id');
+        $qLender = $this->entityManager->getRepository(Lender::class)->createQueryBuilder('l')->where('l.name = :name');
 
-        $qLender = $this->entityManager
-            ->getRepository(Lender::class)
-            ->createQueryBuilder('l')
-            ->where('l.name = :name');
+        try {
+            $notLender = $qLender->setParameter('name', 'Not Lender')->getQuery()->getSingleResult();
+        }
+        catch (NoResultException $e) {
+            $notLender = new Lender();
+            $notLender->setName('Not Lender');
+            $this->entityManager->persist($notLender);
+            $this->entityManager->flush();
+        }
 
-        $notLender = $qLender->setParameter('name', 'Not Lender')->getQuery()->getSingleResult();
-
+        $this->countUpdate = 0;
+        $this->countCreate = 0;
         $this->sync->fetch(function($meta, $data) use ($qUser, $qLender, $notLender) {
             if ($meta['type'] === 'contact' && isset($data['id'], $data['email'])) {
                 if (isset($data['custom_fields']['Sub-Company Name (DBA)'])) {
@@ -83,11 +82,13 @@ class SyncCommand extends Command
                     // Update user
                     try {
                         $user = $qUser->setParameter('id', $data['id'])->getQuery()->getSingleResult();
+                        $this->countCreate++;
                     }
-                        // New user
+                    // New user
                     catch (NoResultException $e) {
                         $user = new User;
                         $user->setPassword(self::DEFAULT_PASSWORD);
+                        $this->countUpdate++;
                     }
                     $user->setBaseId($data['id']);
                     $user->setEmail($data['email']);
@@ -109,6 +110,6 @@ class SyncCommand extends Command
             }
         });
 
-        $output->writeln('Finished sync');
+        $output->writeln(sprintf('Finished sync. Created: %s. Updated: %s ', $this->countCreate, $this->countUpdate));
     }
 }
--- src/LO/Console/Command/console.php
+++ src/LO/Console/Command/console.php
@@ -10,8 +10,8 @@ use Symfony\Component\Console\Application;
 $app = new \LO\Application(['prod', 'config'], __DIR__.'/../../../../config/');
 $app->bootstrap()->initRoutes();
 $app->boot();
-$em = $app->getEntityManager();
+
 $application = new Application();
-$application->add(new SyncCommand($app, $em));
-$application->add(new UsersImportCommand($em));
+$application->add(new SyncCommand($app));
+$application->add(new UsersImportCommand($app->getEntityManager()));
 $application->run();
