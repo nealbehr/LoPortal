227bdd5191580448b13197d22ad1217658298a1f
Eugene Lysenko <zhenyaaynehz@gmail.com>
2016-02-27 13:19:26 +0200

    Update deployment credentials

--- web/js/service/helper.js
+++ web/js/service/helper.js
@@ -4,68 +4,6 @@
 
     var helperService = angular.module('helperService', ['headColumnModule', 'pictureModule']);
 
-    helperService.directive('loDashboardRow', ['$timeout', 'tableHeadColSample', function($timeout, tableHeadColSample){
-        return { restrict: 'EA',
-            templateUrl: '/partials/dashboard.row',
-            scope: {
-                items: '=loItems',
-                id:    '@loId',
-                title: '@loTitle',
-                isExpand: '=loIsExpand',
-                state: "@loState"
-            },
-            link: function(scope, element, attrs, controllers){
-                scope.requestType = settings.queue.type;
-                function params (key, title){
-                    this.key = key;
-                    this.title = title;
-                }
-                params.prototype.directionKey     = 'reverse';
-                params.prototype.scope     = scope;
-                params.prototype.sortKey          = 'predicate';
-                params.prototype.defaultDirection = true;
-                params.prototype.defaultSortKey   = 'created_at';
-
-
-                scope.headParams = [
-                    new tableHeadColSample(new params("created_at.date", "Created")),
-                    new tableHeadColSample(new params("address", "Property Address")),
-                    new tableHeadColSample(new params("request_type", "Type"))
-                ];
-
-                scope.predicate = scope.headParams[0].key;
-                scope.reverse = true;
-
-                $timeout(function(){
-//                    angular.element("#" + scope.id + " > table").tablesorter();
-                });
-
-                scope.$watch("isExpand", function(newValue){
-                    if(newValue){
-                        $('#' + scope.id).collapse('show');
-                    }
-                });
-
-                scope.isApprovedProperty = function(item){
-                    return item.request_type == this.requestType.propertyApproval && item.state == settings.queue.state.approved;
-                };
-                scope.isApprovedFlyer= function(item){
-                    return item.request_type == this.requestType.flyer && item.state == settings.queue.state.approved
-                };
-                scope.canCancel = function(item){
-                    return item.state == settings.queue.state.requested || item.state == settings.queue.state.draft;
-                };
-                scope.isComplete = function(item){
-                    return item.state == settings.queue.state.draft;
-                };
-                scope.isDeclined = function(item){
-                    return item.state == settings.queue.state.declined;
-                };
-
-            }
-        }
-    }]);
-
     helperService.directive('validFile', function () {
         return {
             require: 'ngModel',
@@ -178,20 +116,6 @@
                     return nameFormat.test(viewValue);
                 }
 
-            }
-        };
-    }]);
-
-    helperService.directive('loMessageContainer', ['sessionMessages', '$compile', function(sessionMessages){
-        return {
-            restrict: 'EA',
-            templateUrl: '/partials/session.messages',
-            link: function(scope, elm, attrs, ctrl) {
-                scope.$on('renderSessionMesages', function () {
-                    scope.messages = sessionMessages.get();
-                });
-
-                scope.messages = sessionMessages.get();
             }
         };
     }]);
